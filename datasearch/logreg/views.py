29013ec70ece668bee0b521f1386b080a9e7c4bd
Sishxo <10180657@mail.ecust.edu.cn>
2023-11-09 23:27:28 +0800

    finish nav bar

--- datasearch/logreg/views.py
+++ datasearch/logreg/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.template import loader
+import json
 
 def login(request):
+    if request.method=="POST":
+        data = json.loads(request.body.decode())
+        
+        username=data["username"]
+        password=data["password"]
+        print(username,password)
+        if username and password:
+            username=username.strip()
+            
     template = loader.get_template('logreg.html')
     return HttpResponse(template.render())
 
